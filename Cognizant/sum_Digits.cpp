b0987c7b5f2453b067e9ee39df990b763a073025
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2021-11-28 20:45:28 +0530

    sum of digits

---
+++ Cognizant/sum_Digits.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define eff ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define vi vector<int>
+
+signed main(){
+    eff;
+    int n;
+    cin>>n;
+    cout<<floor(log10(n))+1;
+    return 0;
+}
