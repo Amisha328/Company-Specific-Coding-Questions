9f64b05d4e80514b95706aadc5ad2d47b88fceb1
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2021-12-28 16:30:26 +0530

    Update CountVowelConsonents.java

--- TCS Xplore Java/CountVowelConsonents.java
+++ TCS Xplore Java/CountVowelConsonents.java
@@ -25,7 +25,7 @@ class MyClass
          for(int i = 0; i < s.length(); i++)
          {
              char ch = s.charAt(i);
-           // check vowels
+            // check vowels
              if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
          		 ++v;
 
