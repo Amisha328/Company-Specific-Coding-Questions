88458cf08d5479e93c9ae79afee796d39b731271
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2021-12-28 11:07:48 +0530

    updated code

--- TCS Xplore Java/CountVowelConsonents.java
+++ TCS Xplore Java/CountVowelConsonents.java
@@ -6,19 +6,21 @@ class MyClass
          Scanner sn = new Scanner(System.in);
          String s = sn.next();
          int v = 0, c = 0;
+         s = s.toLowerCase();
+        
          for(int i = 0; i < s.length(); i++)
          {
-           char ch = s.charAt(i);
-           if(Character.isLetter(ch))
-           {
-             ch = Character.toLowerCase(ch);
-             if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-               v++;
-             else
-               c++;
-           }
-         }
+             char ch = s.charAt(i);
+           // check vowels
+             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
+         		 ++v;
+
+            // check if character is in between a to z
+            else if ((ch >= 'a' && ch <= 'z')) 
+              	++c;
+        }
              
         System.out.print("Number of Vowels: " + v + "\n" + "Number of Consonants: "+ c); 
     } 
-}            
+}
+            
