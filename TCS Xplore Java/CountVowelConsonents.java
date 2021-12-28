87c533de1e1ed9c67675bb3241773437ef77b151
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2021-12-28 16:25:42 +0530

    Added question

--- TCS Xplore Java/CountVowelConsonents.java
+++ TCS Xplore Java/CountVowelConsonents.java
@@ -1,3 +1,17 @@
+/*
+In the main method, read a String (which may have alphabets along with numeric digits) and print the number of vowel and consonants (any alphabet apart from vowel is a consonant) present in the given String.
+Note: The output should be printed in the same format as mentioned in the sample output.
+
+Consider below sample input and output:
+
+Input:
+Welcome123
+
+Output:
+Number of Vowels: 3
+Number of Consonants: 4
+*/
+
 import java.util.*; 
 class MyClass 
 { 
