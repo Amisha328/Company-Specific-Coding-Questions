e2b7af5845fcaec748b018d6ee50d71440dbcd6b
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2021-12-28 16:23:59 +0530

    Added question

--- TCS Xplore Java/reverseString.java
+++ TCS Xplore Java/reverseString.java
@@ -1,3 +1,15 @@
+/*
+In the main method, write code to read a numeric digit(without any alphabets or special characters) using Scanner and print it in the reverse sequence as they appear in the input.
+
+Consider below sample input and output:
+
+Input:
+12345
+
+Output:
+Reverse of the number is 54321
+*/
+
 import java.util.*; 
 class MyClass 
 { 
